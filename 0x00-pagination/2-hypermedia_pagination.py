f022fcb97febc43576cfe39b7befe37c7d22aa70
judeiwuji <judeiwuji@gmail.com>
2023-04-15 00:50:26 -0700

    annotations update

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -2,7 +2,7 @@
 """Module: Hypermedia pagination"""
 import csv
 import math
-from typing import List, Dict
+from typing import List, Dict, Union
 
 
 class Server:
@@ -36,7 +36,7 @@ class Server:
         start_index, end_index = index_range(page, page_size)
         return self.dataset()[start_index:end_index]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Union[int, None]]:
         """Returns a hypermedia data
 
         Args:
