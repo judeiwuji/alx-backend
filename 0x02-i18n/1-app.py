b1f212f1c3cc8edf7a5f3f1460f94a1e603a4906
judeiwuji <judeiwuji@gmail.com>
2023-04-25 15:46:35 +0100

    update

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Module: Flask and Babel App"""
-from flask import Flask
+from flask import Flask, render_template
 from flask_babel import Babel
 
 
@@ -14,3 +14,13 @@ class Config:
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app=app)
+
+
+@app.route('/')
+def index():
+    """Render index page"""
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
