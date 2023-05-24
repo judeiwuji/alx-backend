4785badb84d104a3f29d06f042d5b8e6db0e3a81
judeiwuji <judeiwuji@gmail.com>
2023-05-24 08:15:38 +0100

    Add: Task 6

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,17 @@
+import kue from "kue";
+
+const queue = kue.createQueue();
+const jobData = {
+  phoneNumber: "090350098800",
+  message: "publish app to prod",
+};
+const job = queue.create("push_notification_code", jobData).save((err) => {
+  if (err) {
+    return console.log("Notification job failed");
+  }
+  console.log(`Notification job created: ${job.id}`);
+
+  job.on("completed", () => {
+    console.log("Notification job completed");
+  });
+});
